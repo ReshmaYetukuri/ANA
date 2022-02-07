fbce5ef3e305636094ca11b82dbe50b88b00092d
salmankareem1 <https://github.com/arunnarasimha5/Task2.git>
2022-02-07 15:57:21 +0530

    error debuged from ArnkSwitch

--- frontend/src/components/presentational/atoms/ARNKswitch/ArnkSwitch.tsx
+++ frontend/src/components/presentational/atoms/ARNKswitch/ArnkSwitch.tsx
@@ -3,8 +3,9 @@ import SvgActiveCarPlane from 'assets/svgr-components/ActiveCarPlane';
 import SvgActivePlainCar02 from 'assets/svgr-components/ActivePlainCar02';
 import { useState } from 'react';
 
+
 type ARNKSwitchProps = {
-  onClick: (state: boolean) => void;
+  onClick: (stateVar: boolean) => void;
 };
 
 const Button = styled.button`
@@ -19,8 +20,8 @@ export const ARNKSwitch: React.FC<ARNKSwitchProps> = ({
 }) => {
   const [state, setState] = useState(true);
 
-  const changeOption = (state: boolean) => {
-    if (state === false) {
+  const changeOption = (stateVar:boolean) => {
+    if (stateVar === false) {
       setState(true);
     } else {
       setState(false);
