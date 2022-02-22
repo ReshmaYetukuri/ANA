71b8c2004c98c9daee72d005be431bbc3e3ec603
Arunraj A V <Arunraj.Alavattoli@ibsplc.com>
2022-02-22 14:45:07 +0530

    short keys restriction service added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
 import 'App.scss';
+import { ShortKeysRestrictService } from 'modules/service/shortkeys.restrict.service';
+import { useEffect } from 'react';
 
-export const App = () => <></>;
+export const App = () => {
+  // common function to restict broswer actions etc. do NOT remove
+  useEffect(() => {
+    ShortKeysRestrictService.restrictBrowserActions();
+  }, []);
+
+  return <></>
+};
 
 export default App;
---
+++ frontend/src/modules/service/shortkeys.restrict.service.tsx
@@ -0,0 +1,43 @@
+
+const restrictBrowserActions = () => {
+  document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+  });
+  document.addEventListener(
+    "keydown",
+    (e) => {
+      // remove 73 to enable inspect
+      if (
+        [65, 70, 72, 73, 74, 76, 78, 80, 83, 84, 87].includes(e.keyCode) &&
+        (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)
+      ) {
+        e.preventDefault();
+      }
+    },
+    false
+  );
+  // prevent browser task manager
+  document.addEventListener(
+    "keydown",
+    (e) => {
+      if ([27].includes(e.keyCode) && e.shiftKey) {
+        e.preventDefault();
+      }
+    },
+    false
+  );
+
+  document.addEventListener(
+    "mousedown",
+    (event) => {
+      if (event.detail > 1) {
+        event.preventDefault();
+      }
+    },
+    false
+  );
+};
+
+export const ShortKeysRestrictService = {
+  restrictBrowserActions,
+};
